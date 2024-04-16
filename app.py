39b4f45435d75040df2cdd704716168c45897225
juan vilano leyva <juan133109@gmail.com>
2024-04-16 16:15:19 -0500

    Cambie algo del codigo

--- app.py
+++ app.py
@@ -6,25 +6,25 @@ import numpy as np
         
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 
-st.header('Proyecto Sprint 5')
+st.header('Proyecto Sprint 5: Herramientas de desarrollo de software')
 st.write('Prueba: Juan Villano')
 
 
-st.write('Esta es un anális de: ') 
+st.write('Este es un análisis de:') 
 
 
-hist_checkbox = st.checkbox('Histograma de millaje de vehiculos', key= 'his_checkbox') # crear un botón
-scatter_checkbox = st.checkbox('Diagrama de dispersion de precio vs millaje', key= 'scatter_checkbox')       
+hist_checkbox = st.checkbox('Histograma de millas de vehículos', key= 'his_checkbox') # crear un botón
+scatter_checkbox = st.checkbox('Diagrama de dispersión de precio vs millas', key= 'scatter_checkbox')       
 
 #Verificar que la casilla de verificacion este seleccionada
 
 if hist_checkbox:
     #crear un histograma
-    fig = px.histogram(car_data, x='odometer', title='Histograma de millaje de vehiculos')
+    fig = px.histogram(car_data, x='odometer', title='Histograma de millas de vehículos')
     #Mostrar un grafico ploty interctivo
     st.plotly_chart(fig, use_container_width=True)
     #Escribir mensaje
-    st.write('Acontinuacion algunas estadisticas descriptivas sobre la variable millaje de los vehiculos')
+    st.write('A continuación, algunas estadísticas descriptivas sobre la variable millas de los vehículos.')
     #Calcular y mostrar la media, la mediana y la desviacion estandar
     st.write(f"Media: {car_data['odometer'].mean():,.0f}, "
              f"Mediana: {car_data['odometer'].median():,.0f}, "
@@ -32,11 +32,11 @@ if hist_checkbox:
     
 if scatter_checkbox:
     #crear un grafico de dispersion con plotly Express
-    fig2 = px.scatter(car_data, x='odometer', y= 'price', title='Diagrama de dispersion de precio vs millaje')
+    fig2 = px.scatter(car_data, x='odometer', y= 'price', title='Diagrama de dispersión de precio vs millas')
     #Mostrar un grafico usando st.plotly_chart()
     st.plotly_chart(fig2, use_container_width=True)
     #Escribir mensaje
-    st.write('Precio y millaje se encuentran inversamente correlacionados.')
+    st.write('Precio y millas se encuentran inversamente correlacionados.')
     #Calcular coeficiente de correlacion y mostrar con 3 decimales
     correlation = car_data['odometer'].corr(car_data['price'])
     st.write(f"Coeficiente de correlación: {correlation:.3f}")
